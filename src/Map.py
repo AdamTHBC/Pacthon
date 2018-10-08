f93c866d2092e3c2b29c695059fed94bcec8033c
orlow-kamil <orlow.kamil@gmail.com>
2018-10-08 22:17:49 +0200

    Init class Map

---
+++ src/Map.py
@@ -0,0 +1,37 @@
+from Important import max_x,max_y
+
+
+class Map:
+    def __init__(self):
+        "nothing"
+
+    def draw(self, hero, objects):
+        y = 0
+        print("/--------------------\\")
+        while (y < max_y):
+            x = 0
+            print("|",end='', flush=True)
+            while (x < max_x):
+                if (x == hero.x and y == hero.y):
+                    print("H", end='', flush=True)
+                else:
+                    flag = True
+                    for i in objects:
+                        if (x == i.x and y == i.y):
+                            print("O", end='', flush=True)
+                            flag = False
+                    if (flag):
+                        print(" ", end='', flush=True)
+                    # print("(", x, ", ", y, ") ", end='', flush=True)
+                x = x + 1
+            print("|")
+            y = y + 1
+
+        print("\\--------------------/")
+
+    def collision(self, hero, objects):
+        for i in objects:
+            if (hero.x == i.x and hero.y == i.y):
+                print("munch!")
+                objects.remove(i)
+                hero.eat()
