8a8fd938fb648eb64e0f6f722622af4edc2cd36b
orlow-kamil <orlow.kamil@gmail.com>
2018-10-10 22:51:52 +0200

    Test module curses -> ex. 2

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-from curses import initscr,endwin,wrapper
+from curses import initscr,endwin,wrapper,A_REVERSE,A_BOLD
 
 def main(stdscr):
     stdscr = initscr()
 
-    for i in range(0,50):
-        stdscr.refresh()
-        stdscr.addstr(10,i,"Hello!")
-        stdscr.getch()
+    max_y,max_x = stdscr.getmaxyx()
+
+    stdscr.attron(A_REVERSE)
+    stdscr.addstr(int(max_y/2)-1,int(max_x/2),'Hello!',A_BOLD)
+    stdscr.addstr(int(max_y/2),int(max_x/2),'Hello!',)
+    stdscr.attroff(A_REVERSE)
+    stdscr.addstr(int(max_y/2)+1,int(max_x/2),'Hello!')
+
+    stdscr.getch()
 
 
     endwin()
