c4ff2b3b28c68e574c1f410dfffe82d23b88c4a8
Kamil Orłow <orlow.kamil@gmail.com>
2018-10-20 11:32:16 +0200

    Test module curses -> 7

--- test.py
+++ test.py
@@ -8,19 +8,35 @@ def main():
     curs_set(False)
     keypad(stdscr,True)
 
-    window = newwin(10,25,3,3)
+    window = newwin(3,20,5,5)
     box(window)
-    
     wmove(window,1,1)
-    waddstr(window,"Hello!")
+    waddstr(window, "Hey you!")
+
+    window2 = newwin(3,20,4,4)
+    box(window2)
+    wmove(window2,1,1)
+    waddstr(window2, "Hey you second!")
     
+
+    panel = new_panel(window)
+    panel2 = new_panel(window2)
+
+    #move_panel(panel,10,30)
+    top_panel(panel)
+    panel
+
+    
+
 
     running = True
     while (running):
-        key = wgetch(window)
+        key = getch()
         if (key == 27):
             running = False
             break
+        update_panels()
+        doupdate()
 
 
     endwin()
